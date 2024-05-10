2886db2b7565b25956f49489d6e97bfea8cb3fed
dattm27 <dattran2003.ttn@gmail.com>
2024-05-10 16:46:53 +0700

    show  admin dashboard done

--- shoes-store/src/main/java/com/shoesstore/controller/AdminController.java
+++ shoes-store/src/main/java/com/shoesstore/controller/AdminController.java
@@ -1,14 +1,22 @@
 package com.shoesstore.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shoesstore.model.CustomUserDetails;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 	@GetMapping("/dashboard")
-	public String showDashboard() {
-		return "admin/dashboard";
+	public String showDashboard(Model model) {
+		// Lấy thông tin người dùng từ session để lấy thông tin công ty
+		CustomUserDetails currentUser = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication()
+				.getPrincipal();
+		model.addAttribute("username", currentUser.getFullName());	
+		return "admin/layout";
 	}
 }
