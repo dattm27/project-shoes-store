afe556330c5367926516ea268a7ce587813f11de
dattm27 <dattran2003.ttn@gmail.com>
2024-05-26 16:35:40 +0700

    display cart done

--- shoes-store/src/main/java/com/shoesstore/controller/CartController.java
+++ shoes-store/src/main/java/com/shoesstore/controller/CartController.java
@@ -1,10 +1,14 @@
 package com.shoesstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,48 +24,63 @@ public class CartController {
 
 	@Autowired
 	private ProductService productService;
-	
+
 	@Autowired
 	private ProductSizeService sizeService;
-	
+
 	@Autowired
 	private CartItemService cartItemService;
-	
-	
-	
+
 	@PostMapping("/add-to-cart")
 	@ResponseBody
 	public ResponseEntity<Object> addToCart(HttpServletRequest request) {
-		//lấy thông tin khách
+		// lấy thông tin khách
 		// Lấy ra người dùng hiện tại
-				com.shoesstore.model.CustomUserDetails currentUser = (com.shoesstore.model.CustomUserDetails) SecurityContextHolder.getContext().getAuthentication()
-						.getPrincipal();
+		com.shoesstore.model.CustomUserDetails currentUser = (com.shoesstore.model.CustomUserDetails) SecurityContextHolder
+				.getContext().getAuthentication().getPrincipal();
 		int customer_id = currentUser.getId();
-		
-		//lấy thông tin sản phẩm 
+
+		// lấy thông tin sản phẩm
 		int product_id = Integer.parseInt(request.getParameter("product_id"));
 		int size_id = Integer.parseInt(request.getParameter("size_id"));
-		
-		//tạo ra cart item mới
-		CartItem cartItem = new CartItem(currentUser.getUser(), productService.findById(product_id), sizeService.getSizeById(size_id), 1);
-		//System.out.println("add to cart " + customer_id + " " + product_id+ " " + size_id);
-		if (cartItemService.addCartItem(cartItem)!= null) 	return ResponseEntity.ok().body(null);
-		else return  ResponseEntity.internalServerError().body(null);
-		
-	
+
+		// tạo ra cart item mới
+		CartItem cartItem = new CartItem(currentUser.getUser(), productService.findById(product_id),
+				sizeService.getSizeById(size_id), 1);
+		// System.out.println("add to cart " + customer_id + " " + product_id+ " " +
+		// size_id);
+		if (cartItemService.addCartItem(cartItem) != null)
+			return ResponseEntity.ok().body(null);
+		else
+			return ResponseEntity.internalServerError().body(null);
+
 	}
-	
-	//cho biết có bao  nhiêu sản phẩm trong giỏ hàng
+
+	// cho biết có bao nhiêu sản phẩm trong giỏ hàng
 	@PostMapping("/cart-header")
 	@ResponseBody
 	public ResponseEntity<Object> getCartHeader() {
 		// Lấy ra người dùng hiện tại
-		com.shoesstore.model.CustomUserDetails currentUser = (com.shoesstore.model.CustomUserDetails) SecurityContextHolder.getContext().getAuthentication()
-				.getPrincipal();
+		com.shoesstore.model.CustomUserDetails currentUser = (com.shoesstore.model.CustomUserDetails) SecurityContextHolder
+				.getContext().getAuthentication().getPrincipal();
 		int size = cartItemService.getCartItemsByUser(currentUser.getUser()).size();
 		System.out.print(size);
 		return ResponseEntity.ok().body(size);
 	}
-	
-	
+
+	// xem giỏ hàng
+	@GetMapping("/cart")
+	public String seeCart(Model model) {
+		// Lấy ra người dùng hiện tại
+		com.shoesstore.model.CustomUserDetails currentUser = (com.shoesstore.model.CustomUserDetails) SecurityContextHolder
+				.getContext().getAuthentication().getPrincipal();
+		List<CartItem> cartItems= cartItemService.getCartItemsByUser(currentUser.getUser());
+		if (cartItems.size() > 0) {
+			model.addAttribute("cartItems",cartItems);
+			return "shopper/cart";
+		}
+			
+		else
+			return "shopper/empty-cart";
+	}
 }
